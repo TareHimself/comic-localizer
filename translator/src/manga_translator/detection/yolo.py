fix: update pixel calculation and enhance detection/segmentation class methods for improved functionality

translator/src/manga_translator/detection/yolo.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from manga_translator.core.constants import DetectionType
 from manga_translator.core.plugin import (
     Detector,
     DetectionResult,
@@ -30,6 +31,16 @@ class YoloDetector(Detector):
         self.iou = iou
         self.skip_free_text = skip_free_text
 
+    @staticmethod
+    def conv_cls(cls: int):
+        if cls == DetectionType.TextInBubble:
+            return DetectionType.TextInBubble
+
+        if cls == DetectionType.TextOnPage:
+            return DetectionType.TextOnPage
+
+        return DetectionType.TextOnPage
+
     def predict(self, batch: list[np.ndarray]):
         with torch.inference_mode():
             results = []
@@ -50,7 +61,11 @@ class YoloDetector(Detector):
                     # depending on model accuracy I have noticed some text bubbles are detected as free text, will need to do custom nms to catch this since we trust text_bubble more than text_free
                     if cls > 0 and self.skip_free_text:
                         continue
-                    result.append(DetectionResult(cls.item(), bbox, conf.item()))
+                    result.append(
+                        DetectionResult(
+                            YoloDetector.conv_cls(cls.item()), bbox, conf.item()
+                        )
+                    )
 
                 results.append(result)
 

translator/src/manga_translator/segmentation/yolo.py
@@ -1,3 +1,4 @@
+from manga_translator.core.constants import SegmentationType
 from manga_translator.core.plugin import (
     PluginArgument,
     PytorchDevicePluginArgument,
@@ -20,6 +21,13 @@ class YoloSegmenter(Segmenter):
         self.model = YOLO(model=model_path, verbose=False)
         self.device = device
 
+    @staticmethod
+    def conv_cls(cls: int):
+        if cls == SegmentationType.Text:
+            return SegmentationType.Text
+
+        return SegmentationType.Text
+
     def predict(self, batch):
         with torch.inference_mode():
             results = []
@@ -40,7 +48,9 @@ class YoloSegmenter(Segmenter):
                     for mask, cls, conf in zip(masks, classes, confidence):
                         result.append(
                             SegmentationResult(
-                                cls.item(), mask.astype(int), conf.item()
+                                YoloSegmenter.conv_cls(cls.item()),
+                                mask.astype(int),
+                                conf.item(),
                             )
                         )
 