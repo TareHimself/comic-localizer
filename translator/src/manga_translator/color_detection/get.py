e273b9896d5f2a8a7bf82fad2255a175e36476c4
Oyintare Ebelo <75279482+TareHimself@users.noreply.github.com>
2026-04-08 14:58:12 -0700

    feat: implement BasicColorDetector for color detection and update color detector registry fix: enhance docker run command and improve bounding box computation in utils

--- translator/src/manga_translator/color_detection/get.py
+++ translator/src/manga_translator/color_detection/get.py
@@ -1,10 +1,11 @@
+from manga_translator.color_detection.basic import BasicColorDetector
 from manga_translator.core.plugin import ColorDetector
 from manga_translator.color_detection.openai import OpenAiColorDetector
 
 _color_detection_data = list(
     filter(
         lambda a: a.is_valid(),
-        [ColorDetector, OpenAiColorDetector],
+        [BasicColorDetector, OpenAiColorDetector],
     )
 )
 
